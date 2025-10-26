c3d8e20c3295e4842571af43dc317de9367ce6f8
Maksym Pulinets <maximus-ms@ukr.net>
2025-10-26 08:37:27 +0200

    Refactor main.py to enable model loading and prediction functionality, update application versioning, and improve prediction response format.

--- mlflow-inference-api/app/main.py
+++ mlflow-inference-api/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
-# import mlflow.pyfunc
-# import pandas as pd
+import mlflow.pyfunc
+import pandas as pd
 import os
 
 app = FastAPI()
@@ -10,27 +10,26 @@ MODEL_NAME = os.getenv("MODEL_NAME")
 MODEL_STAGE = os.getenv("MODEL_STAGE")
 MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI")
 MODEL_URI = f"models:/{MODEL_NAME}/{MODEL_STAGE}"
-MODEL_VERSION = os.getenv("MODEL_VERSION", "1.0.2")
+APP_VERSION = "2.0.0"
 
 print(f"MLFLOW_TRACKING_URI: {MLFLOW_TRACKING_URI}")
 print(f"MODEL_NAME: {MODEL_NAME}")
 print(f"MODEL_STAGE: {MODEL_STAGE}")
 print(f"MODEL_URI: {MODEL_URI}")
-print(f"MODEL_VERSION: {MODEL_VERSION}")
+print(f"APP_VERSION: {APP_VERSION}")
 
-# model = mlflow.pyfunc.load_model(model_uri=MODEL_URI)
-# print(f"Model: {model}")
+model = mlflow.pyfunc.load_model(model_uri=MODEL_URI)
+print(f"Model: {model}")
 
 class InputData(BaseModel):
     features: list
 
 @app.post("/predict")
 def predict(data: InputData):
-    # df = pd.DataFrame(body["instances"])
-    # prediction = model.predict(df)
-    # return {"predictions": prediction.tolist()}
-    return {"predictions": data.features + [42]}
+    df = pd.DataFrame(data.features)
+    prediction = model.predict(df)
+    return {"predictions": prediction.tolist()}
 
 @app.post("/version")
 def version():
-    return {"version": MODEL_VERSION}
+    return {"version": APP_VERSION}
